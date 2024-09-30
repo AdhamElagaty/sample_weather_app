9f39fc639a8297b47d9a30935c8371091db27ced
Adham Elagaty <adhamalagaty@gmail.com>
2024-09-30 10:15:06 +0300

    Create SplashView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sample_weather_app/views/cubits/weather_cubit/weather_cubit.dart';
+import 'package:sample_weather_app/views/splash_view.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -9,6 +12,15 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (context) => WeatherCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: const SplashView(),
+      ),
+    );
   }
 }
